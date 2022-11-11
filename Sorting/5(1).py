a8c8b3f2dc876feb6cb5ceed215817aafe9303c9
Sobitora <web_124@hotmail.com>
2022-11-11 22:02:57 +0700

    update sort

--- Sorting/5(1).py
+++ Sorting/5(1).py
@@ -1,12 +1,11 @@
-checksubset = False
-
-def sumsubset(count,inp,newlist,num,sumlist:list,check):
+def sumsubset(count,inp,newlist,num,sumlist:list):
     if count != 0:
         newlist.append(inp[0])
-        sumsubset(count-1,inp[1:],newlist,num,sumlist,check)
+        sumsubset(count-1,inp[1:],newlist,num,sumlist)
         newlist.pop(-1)
         if len(inp) != count:
-            sumsubset(count,inp[1:],newlist,num,sumlist,check)
+            sumsubset(count,inp[1:],newlist,num,sumlist)
+        return sumlist
     elif len(inp) != 0:
         newlist.append(inp[0])
         if sum(newlist) == num :
@@ -14,13 +13,9 @@ def sumsubset(count,inp,newlist,num,sumlist:list,check):
             sumlist[len(sumlist)-1]=list(newlist)
             sortlist(sumlist[len(sumlist)-1])
             sortiteminlist(sumlist)
-            global checksubset
-            checksubset = True
-        if newlist == check:
-            for i in sumlist:
-                print(i)
         newlist.pop(-1)
-        sumsubset(count,inp[1:],newlist,num,sumlist,check)
+        sumsubset(count,inp[1:],newlist,num,sumlist)
+        return sumlist
 
 
 def sortlist(l):
@@ -53,10 +48,13 @@ def sortiteminlist(l):
 inp = input("Enter Input : ").split("/")
 num = int(inp[0])
 inp = [int(i) for i in inp[1].split(" ")]
-sumlist = []
+check = False
 for i in range(len(inp)):
-    sumsubset(i,inp,[],num,[],inp[-(i+1):])
-if checksubset == False:
+    sumlist = sumsubset(i,inp,[],num,[])
+    for j in sumlist:
+        check = True
+        print(j)
+if check == False:
     print("No Subset")
 
 
